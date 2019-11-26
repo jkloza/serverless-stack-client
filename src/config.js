0811d5536bd9dda2b2c009681d9c72cf198e9577
Juli Kloza <julikloza@Julis-MacBook-Pro.local>
2019-11-26 10:35:07 -0500

    setting up app to amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "aws-fun-notes-storage"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ia7ca7pcfi.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "YO9FDlI13",
+    APP_CLIENT_ID: "1nsvg6sn9fl98ukp9ffsc8mm0d",
+    IDENTITY_POOL_ID: "98786e28-f389-4d90-b948-c9ed2adedf40"
+  }
+};
---
+++ src/containers/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return(
+    <div style={{ paddingTop: 100, textAlign: 'center' }}>
+      <h3>Sorry, page not found!</h3>
+    </div>
+  )
+}
